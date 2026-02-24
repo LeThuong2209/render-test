8a7be779dcac5dd6d0a6751aa298314aaf96df68
Lê Thường <trandung@192.168.1.106>
2026-02-25 00:18:35 +0700

    fix syntax error

--- back-end-notes/index.js
+++ back-end-notes/index.js
@@ -1,7 +1,7 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const Note = require("./models/note");
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const Note = require('./models/note')
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -15,18 +15,18 @@ app.use(requestLogger)
 app.use(express.static('dist'))
 app.use(express.json())
 
-app.get("/api/notes", (request, response) => {
+app.get('/api/notes', (request, response) => {
   Note.find({}).then((notes) => {
-    response.json(notes);
-  });
-});
+    response.json(notes)
+  })
+})
 
-app.get("/api/notes/:id", (request, response, next) => {
-  const id = request.params.id;
+app.get('/api/notes/:id', (request, response, next) => {
+  const id = request.params.id
   Note.findByID(id)
     .then((note) => {
       if (note){
-        response.json(note);
+        response.json(note)
       }
       else {
         response.status(404).end()
@@ -34,39 +34,39 @@ app.get("/api/notes/:id", (request, response, next) => {
     })
     .catch(error => {
       next(error)
-    });
-});
+    })
+})
 
-app.delete("/api/notes/:id", (request, response, next) => {
+app.delete('/api/notes/:id', (request, response, next) => {
   const id = request.params.id
 
   Note.findByIdAndDelete(id)
-    .then((note) => {
+    .then(() => {
       response.status(204).end()
     })
     .catch(error => next(error))
 })
 
-app.post("/api/notes", (request, response, next) => {
-  const body = request.body;
+app.post('/api/notes', (request, response, next) => {
+  const body = request.body
 
   if (!body.content) {
     return response.status(400).json({
-      error: "Content missing",
-    }); 
+      error: 'Content missing',
+    })
   }
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
-  });
+  })
 
   note.save()
     .then((savedNote) => {
-      response.json(savedNote);
+      response.json(savedNote)
     })
-    .catch(error => next(error));
-});
+    .catch(error => next(error))
+})
 
 app.put('/api/notes/:id', (request, response, next) => {
   const {content, important} = request.body
@@ -80,10 +80,10 @@ app.put('/api/notes/:id', (request, response, next) => {
       note.content = content
       note.important = important
 
-      return note.save().then(savedNote => {
-        response.json(savedNote)
-      })
-    })
+      return note.save()
+        .then(savedNote => {
+          response.json(savedNote)
+        })})
     .catch(error => next(error))
 })
 
@@ -108,7 +108,7 @@ const errorHandler = (error, request, response, next) => {
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
